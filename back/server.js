48486cb47e454e5c125f593185ed19050cfb947f
2nio <antoniogeorgetoader@yahoo.ro>
2024-08-25 00:27:31 +0300

    cors update

--- back/server.js
+++ back/server.js
@@ -17,7 +17,7 @@ const { createClient, getAllClients, getClient, editClient, deleteClient } = req
 
 const app = express();
 app.use((req, res, next) => {
-    res.header('Access-Control-Allow-Origin', '*'); // Allow your frontend domain
+    res.header('Access-Control-Allow-Origin', 'https://fluxloop.vercel.app'); // Allow your frontend domain
     res.header('Access-Control-Allow-Methods', 'GET,POST,PUT,DELETE,OPTIONS'); // Allow specific HTTP methods
     res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization'); // Allow specific headers
     res.header('Access-Control-Allow-Credentials', 'true'); // Allow credentials (e.g., cookies)
