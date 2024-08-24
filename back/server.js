bf8613aff348a032ef944ab10da6b95baa1dc061
2nio <antoniogeorgetoader@yahoo.ro>
2024-08-25 00:04:05 +0300

    cors update

--- back/server.js
+++ back/server.js
@@ -13,10 +13,22 @@ const { createIncome, getAllIncome, deleteIncome, getIncome, editIncome } = requ
 const { createBill, getAllBills, deleteBill, getBill, editBill } = require('./controllers/billController');
 const { createPayment, getAllPayments, deletePayment, getPayment, editPayment } = require('./controllers/paymentController');
 const { createClient, getAllClients, getClient, editClient, deleteClient } = require('./controllers/clientController');
-const cors = require('./middleware/cors');
+//const cors = require('./middleware/cors');
 
 const app = express();
-app.use(cors)
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', 'https://fluxloop.vercel.app'); // Allow your frontend domain
+    res.header('Access-Control-Allow-Methods', 'GET,POST,PUT,DELETE,OPTIONS'); // Allow specific HTTP methods
+    res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization'); // Allow specific headers
+    res.header('Access-Control-Allow-Credentials', 'true'); // Allow credentials (e.g., cookies)
+
+    // Handle preflight requests
+    if (req.method === 'OPTIONS') {
+        return res.sendStatus(200); // Respond with 200 OK
+    }
+
+    next();
+});
 /* app.use(cors({
     origin: ['https://fluxloop.vercel.app'],
     methods: ['POST', 'GET', 'DELETE', 'PUT'],
